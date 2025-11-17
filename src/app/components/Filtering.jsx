a2a277dfdb64fe9649b7f80f271e7979f7051c32
SAMANTHA PARKES <slmillward@outlook.com>
2025-11-17 20:02:23 +0000

    filtering by type and tags

---
+++ src/app/components/Filtering.jsx
@@ -0,0 +1,44 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+
+export default function Filtering({ allTags, allTypes }) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentType = searchParams.get("type") || "";
+  const currentTag = searchParams.get("tag") || "";
+
+  function handleChange(e) {
+    const newType = e.target.name === "type" ? e.target.value : currentType;
+    const newTag = e.target.name === "tag" ? e.target.value : currentTag;
+    router.push(`/workouts?type=${newType}&tag=${newTag}`);
+  }
+
+  return (
+    <div>
+      <form>
+        <label>
+          Type:
+          <select name="type" value={currentType} onChange={handleChange}>
+            <option value="">All</option>
+            {allTypes.map((type) => (
+              <option key={type} value={type}>
+                {type}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label>
+          Tag:
+          <select name="tag" value={currentTag} onChange={handleChange}>
+            <option value="">All</option>
+            {allTags.map((tag) => (
+              <option key={tag.id} value={tag.tag_name}>
+                {tag.tag_name}
+              </option>
+            ))}
+          </select>
+        </label>
+      </form>
+    </div>
+  );
+}
--- src/app/workouts/page.js
+++ src/app/workouts/page.js
@@ -2,32 +2,75 @@ import { db } from "@/utils/utilities";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import Image from "next/image";
+import Filtering from "../components/Filtering";
 
-export default async function Page() {
-  const workouts = (await db.query(`SELECT * FROM workouts`)).rows;
+export default async function Page({ searchParams }) {
   const { isAuthenticated, redirectToSignIn } = await auth();
 
-  if (isAuthenticated == false) {
+  if (!isAuthenticated) {
     redirectToSignIn();
   }
+  const params = await searchParams;
+  const selectedType = params?.type || "";
+  const selectedTag = params?.tag || "";
+
+  console.log("Selected type:", selectedType);
+  console.log("Selected tag:", selectedTag);
+
+  const data = await db.query(
+    `SELECT DISTINCT type FROM workouts ORDER by type ASC`
+  );
+  const allTypes = data.rows.map((row) => row.type);
+  const allTags = (await db.query(`SELECT * FROM tags ORDER BY tag_name ASC`))
+    .rows;
+
+  let workouts;
+
+  if (selectedType && selectedTag) {
+    workouts = (
+      await db.query(
+        `SELECT workouts.* FROM workouts JOIN workouts_tags ON workouts.id = workouts_tags.workout_id JOIN tags ON workouts_tags.tag_id = tags.id WHERE workouts.type = $1 AND tags.tag_name = $2`,
+        [selectedType, selectedTag]
+      )
+    ).rows;
+  } else if (selectedTag) {
+    workouts = (
+      await db.query(
+        `SELECT workouts.* FROM workouts JOIN workouts_tags on workouts.id = workouts_tags.workout_id JOIN tags ON workouts_tags.tag_id = tags.id WHERE tags.tag_name = $1`,
+        [selectedTag]
+      )
+    ).rows;
+  } else if (selectedType) {
+    workouts = (
+      await db.query(`SELECT * FROM workouts WHERE type = $1`, [selectedType])
+    ).rows;
+  } else {
+    workouts = (await db.query(`SELECT * FROM workouts`)).rows;
+  }
+
   return (
     <div className="flex flex-col items-center text-center">
       <p>Choose a workout</p>
+      <Filtering allTypes={allTypes} allTags={allTags} />
       <div className="flex flex-wrap gap-5 mt-12 max-w-sm justify-center md:max-w-3xl">
-        {workouts.map((workout) => (
-          <Link href={`workouts/${workout.id}`} key={workout.id}>
-            <h2>
-              {workout.class} #{workout.episode_no}
-            </h2>
-            <Image
-              src={workout.img_url}
-              alt=""
-              height={200}
-              width={300}
-              className="h-auto"
-            />
-          </Link>
-        ))}
+        {workouts.length > 0 ? (
+          workouts.map((workout) => (
+            <Link href={`/workouts/${workout.id}`} key={workout.id}>
+              <h2>
+                {workout.class} #{workout.episode_no}
+              </h2>
+              <Image
+                src={workout.img_url}
+                alt=""
+                height={200}
+                width={300}
+                className="h-auto"
+              />
+            </Link>
+          ))
+        ) : (
+          <p>No workouts match your filters</p>
+        )}
       </div>
     </div>
   );

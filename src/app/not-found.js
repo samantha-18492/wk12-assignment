8e9f0b5c38d0cbdaa4cafb758803756c853682f8
SAMANTHA PARKES <slmillward@outlook.com>
2025-11-19 09:43:19 +0000

    styling not-found pages

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import { teko } from "./layout";
 
 export default function notFound() {
   return (
-    <div>
-      <h2>Oops! That page doesn&apos;t exist.</h2>
-      <br />
-      <p>Return home to get back on track</p>
-      <Link href="/">Home</Link>
-    </div>
+    <section className="text-center">
+      <h2 className={`${teko.className} text-2xl uppercase`}>Oops! </h2>
+      <p className="my-2">That page doesn&apos;t exist.</p>
+      <p className="my-2">Head home to get moving in the right direction.</p>
+      <Link
+        href="/"
+        className={`${teko.className} bg-flexmills-green text-xl uppercase px-5 pt-2 pb-1 mt-4 inline-block hover:border-2 hover:border-flexmills-black`}
+      >
+        Home
+      </Link>
+    </section>
   );
 }
--- src/app/workouts/[id]/not-found.js
+++ src/app/workouts/[id]/not-found.js
@@ -1,12 +1,18 @@
 import Link from "next/link";
+import { teko } from "@/app/layout";
 
 export default function notFound() {
   return (
-    <div>
-      <h2>We couldn&apos;t find that workout.</h2>
-      <br />
-      <p>Come back soon or browse our existing listings.</p>
-      <Link href="/workouts">Browse</Link>
-    </div>
+    <section className="text-center">
+      <h2 className={`${teko.className} text-2xl uppercase`}>rest day</h2>
+      <p className="my-2">We couldn&apos;t find that workout.</p>
+      <p className="my-2">Check back soon or browse our existing workouts.</p>
+      <Link
+        href="/workouts"
+        className={`${teko.className} bg-flexmills-green text-xl uppercase px-5 pt-3 pb-2 mt-4 inline-block hover:border-2 hover:border-flexmills-black`}
+      >
+        Find a workout
+      </Link>
+    </section>
   );
 }
